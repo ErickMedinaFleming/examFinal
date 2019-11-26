17e98d506f8e1a083e1ed4bd6c30f4a3ee264959
abqu <erick_abqu@hotmail.com>
2019-11-25 19:23:11 -0600

    front changed

--- FRONTEND/js/todo.js
+++ FRONTEND/js/todo.js
@@ -8,7 +8,7 @@ var todos = document.querySelectorAll("input[type=checkbox]");
 
 function loadTodos() {
   $.ajax({
-    url: 'http://localhost:3000/todos',
+    url: 'https://abqustodo.herokuapp.com/todos',
     // url: 'https://tuapp.herokuapp.com/todos',
     headers: {
         'Content-Type':'application/json',
